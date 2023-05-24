2fb951bb4f2828e1ec7ed7e4e1bcd7ba58510420
Thanseefap <59045837+Thanseefap@users.noreply.github.com>
2023-05-24 22:31:39 +0530

    Update and rename api_helper.py to calculator.py

---
+++ calculator.py
@@ -0,0 +1,25 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def calculator():
+    operand1 = int(request.args.get('operand1'))
+    operand2 = int(request.args.get('operand2'))
+    operator = request.args.get('operator')
+
+    if operator == '+':
+        result = operand1 + operand2
+    elif operator == '-':
+        result = operand1 - operand2
+    elif operator == '*':
+        result = operand1 * operand2
+    elif operator == '/':
+        result = operand1 / operand2
+    else:
+        return 'Invalid operator'
+
+    return f'The result is: {result}'
+
+if __name__ == '__main__':
+    app.run(debug=True)
