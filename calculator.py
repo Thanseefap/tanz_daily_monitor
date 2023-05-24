6e9a93e8a03bb8770e5713f80db0ea838b422dc6
Thanseefap <59045837+Thanseefap@users.noreply.github.com>
2023-05-25 00:04:14 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,25 +1,10 @@
-from flask import Flask, request
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def calculator():
-    operand1 = int(request.args.get('operand1'))
-    operand2 = int(request.args.get('operand2'))
-    operator = request.args.get('operator')
-
-    if operator == '+':
-        result = operand1 + operand2
-    elif operator == '-':
-        result = operand1 - operand2
-    elif operator == '*':
-        result = operand1 * operand2
-    elif operator == '/':
-        result = operand1 / operand2
-    else:
-        return 'Invalid operator'
-
-    return f'The result is: {result}'
+@app.route('/')
+def hello():
+    return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
