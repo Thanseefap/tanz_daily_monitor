64a806d92f8a0dc4398891df745584fc4c1f27ed
Thanseefap <59045837+Thanseefap@users.noreply.github.com>
2023-05-25 18:57:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+import boto3
 
-app = Flask(__name__)
+# Create a DynamoDB client
+dynamodb_client = boto3.client('dynamodb')
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+# Define the table name
+table_name = 'login'
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+# Define the data to be added
+item = {
+    'otp': {'458758}
+}
 
+# Add the item to the DynamoDB table
+response = dynamodb_client.put_item(TableName=table_name, Item=item)
+
+# Check the response for success/failure
+if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+    print('Data added to DynamoDB successfully')
+else:
+    print('Failed to add data to DynamoDB')
