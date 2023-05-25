c7c7c56c562a6f84e37ba2c7300bd7e287f6a1cd
Thanseefap <59045837+Thanseefap@users.noreply.github.com>
2023-05-26 00:27:40 +0530

    Update updating_dynamodb.py

--- updating_dynamodb.py
+++ updating_dynamodb.py
@@ -18,7 +18,7 @@ response = dynamodb_client.update_item(
     Key={
         'metrices': {'S': partition_key_value}
     },
-    UpdateExpression='SET value = :value1',
+    UpdateExpression='SET result = :value1',
     ExpressionAttributeValues={
         ':value1': {'S': updated_attribute1}
     }
