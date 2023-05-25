a7d743d9cee222e414521348399117cb120528a1
Thanseefap <59045837+Thanseefap@users.noreply.github.com>
2023-05-26 00:22:01 +0530

    Update updating_dynamodb.py

--- updating_dynamodb.py
+++ updating_dynamodb.py
@@ -1,33 +1,31 @@
-
 import boto3
 
-# Create a DynamoDB client with the desired region
-dynamodb_client = boto3.client('dynamodb', region_name='us-east-1')
+# Create a DynamoDB client
+dynamodb_client = boto3.client('dynamodb')
 
 # Define the table name
 table_name = 'Shoonya'
 
-# Define the key and updated value
-key = 'metrices'
-updated_value = '1111'
+# Define the partition key value
+partition_key_value = 'OTP'
 
-# Update the key-value pair in the DynamoDB table
+# Define the updated values for other attributes
+updated_attribute1 = '1111'
+
+# Update the item using the partition key
 response = dynamodb_client.update_item(
-   TableName=table_name,
-   Key={
-       'metrices': {'S': 'OTP'}  # Assuming 'id' is the primary key
-   },
-   UpdateExpression='SET #key = :value',
-   ExpressionAttributeNames={
-       '#key': key
-   },
-   ExpressionAttributeValues={
-       ':value': {'S': updated_value}
-   }
+    TableName=table_name,
+    Key={
+        'metrices': {'S': partition_key_value}
+    },
+    UpdateExpression='SET attribute1 = :value1',
+    ExpressionAttributeValues={
+        ':value1': {'S': updated_attribute1}
+    }
 )
 
 # Check the response for success/failure
 if response['ResponseMetadata']['HTTPStatusCode'] == 200:
-   print('Key-value pair updated successfully')
+    print('Item updated successfully')
 else:
-   print('Failed to update the key-value pair')
+    print('Failed to update the item')
