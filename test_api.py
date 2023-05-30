1121e331d1546dd2a3993845fb98ae205a3ae549
Thanseefap <59045837+Thanseefap@users.noreply.github.com>
2023-05-30 13:33:15 +0530

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -1,21 +1,4 @@
 from login import shoonya
  
-#enable dbug to see request and responses
-logging.basicConfig(level=logging.DEBUG)
-
-#start of our program
-api = ShoonyaApiPy()
-
-#credentials
-user    = <uid>
-pwd     = <password>
-factor2 = <2nd factor>
-vc      = <vendor code>
-app_key = <secret key>
-imei    = <imei>
-
-#make the api call
-ret = api.login(userid=uid, password=pwd, twoFA=factor2, vendor_code=vc, api_secret=app_key, imei=imei)
-
-print(ret)
-
+client=shoonya(twofa='84485')
+api=client.login()
