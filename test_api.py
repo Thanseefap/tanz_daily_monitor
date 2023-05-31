02f675d3c9598cd99a4153d2a6ba57fd1313fcdc
Thanseefap <59045837+Thanseefap@users.noreply.github.com>
2023-05-31 20:34:06 +0530

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -6,8 +6,8 @@ from datetime import datetime
 otp=input('Enter OTP:')
 client=shoonya(twofa=otp)
 api=client.login()
-scrip=input('Enter the script name')
-stop_loss=float(input('Enter the SL'))
+scrip=input('Enter the script name : ')
+stop_loss=float(input('Enter the SL : '))
 
 
 def Riskmanager(script,percent):
@@ -50,33 +50,18 @@ def Riskmanager(script,percent):
 
 
 
+import time
 
-def update_strategy_performance(scrip,stop_loss):
+def update_strategy_performance(scrip, stop_loss):
     # code to update the performance of the trading strategy
-    show=Riskmanager(scrip,stop_loss)
-    Net_credit=show[4]
-    Net_PL=show[0]
-    CURRENT=show[1]
-    stop_loss=show[2]   
-    margin=show[3]
-    sl_in_cash=stop_loss*(margin/100)
-    strategy_profit_label.config(text=f' Net PL: {CURRENT} and Net Percentage Max Profit : {CURRENT*100/Net_credit}')
-    Profit_label.config(text=f' Percentage PL:- {Net_PL} and Total Premium :- {Net_credit}')
-    stop_loss_label.config(text=f' stop loss %:- {stop_loss} and SL in Cash :- {sl_in_cash}')
-    margin_used.config(text=f' margin_used %: {margin}')
-    last_updated_label.config(text=f'Last Updated: {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
-    root.after(5000, update_strategy_performance)
-    
-#def Day_mtm(script):
-       # df=pd.DataFrame(api.get_positions())
-        #df[['urmtom', 'rpnl']] = df[['urmtom', 'rpnl']].apply(pd.to_numeric)
-        #mtm=df['urmtom'][df['tsym'].str.contains(script)].sum()
-        #booked=df['rpnl'][df['tsym'].str.contains(script)].sum()
-        #X=[mtm+booked,mtm,booked]
-        #return  X
-def calculate_strategy_profit():
-    # code to calculate the profit of the trading strategy
-    return 100.0
+    show = Riskmanager(scrip, stop_loss)
+    Net_credit = show[4]
+    Net_PL = show[0]
+    CURRENT = show[1]
+    stop_loss = show[2]
+    margin = show[3]
+    sl_in_cash = stop_loss * (margin / 100)
+    # Modify the code here to perform the necessary actions with the values, such as saving to a file or database.
 
 def on_entry_button_click():
     # code to execute when the entry button is clicked
@@ -90,11 +75,11 @@ def ATM_price():
     qRes = api.get_quotes('NSE', 'Nifty Fin Service')
     indexLtp = float(qRes['lp'])
     import math
-    mod = int(indexLtp)%100
+    mod = int(indexLtp) % 100
     if mod < 50:
-        atmStrike =  int(math.floor(indexLtp / 100)) * 100
+        atmStrike = int(math.floor(indexLtp / 100)) * 100
     else:
-        atmStrike=  int(math.ceil(indexLtp /100)) * 100
+        atmStrike = int(math.ceil(indexLtp / 100)) * 100
     return atmStrike
 
 def Position():
@@ -106,40 +91,9 @@ def Position():
         pnl += float(i['rpnl'])
     day_m2m = mtm + pnl
     return day_m2m
-  
 
-root = tk.Tk()
-root.title("Algo Trade Monitor")
-
-strategy_profit_label = tk.Label(root, text="Strategy Profit: N/A")
-strategy_profit_label.pack()
-
-
-stop_loss_label = tk.Label(root, text="stop loss %: N/A")
-stop_loss_label.pack()
-
-margin_used = tk.Label(root, text="margin_used : N/A")
-margin_used.pack()
-
-
-
-Profit_label = tk.Label(root, text="P & L: N/A")
-Profit_label.pack()
-
-last_updated_label = tk.Label(root, text="Last Updated: N/A")
-last_updated_label.pack()
-
-entry_button = tk.Button(root, text="Entry", command=on_entry_button_click)
-entry_button.pack()
-
-exit_button = tk.Button(root, text="Exit", command=on_exit_button_click)
-exit_button.pack()
-
-exit_button = tk.Button(root, text="Exit Screen", command=root.destroy)
-exit_button.pack()
-
-update_strategy_performance(scrip,stopp_loss)
-
-root.mainloop()
+while True:
+    update_strategy_performance(scrip, stop_loss)
+    time.sleep(10)  # Delay for 10 seconds
 
 
