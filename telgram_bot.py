4e1d2c142d3a69a42c58cf542c255453f8786d83
Thanseefap <59045837+Thanseefap@users.noreply.github.com>
2023-06-21 00:51:22 +0530

    Update telgram_bot.py

--- telgram_bot.py
+++ telgram_bot.py
@@ -177,7 +177,306 @@ def update_strategy_performance(script, stop_loss):
     return show
     # Modify the code here to perform the necessary actions with the values, such as saving to a file or database.
      
+
+
+
+
+
+global api
+global SL
+global scrip
+global exit
+
+bot = telebot.TeleBot('6066484216:AAFmM3QTtaaR1JDIee71Vs3Aqq95DQNn9yk')
+LOGIN_OTP = 'login_otp'
+SL_UPDATE = 'sl_update'
+
+
+# function to message the data as table 
+
+time.sleep(1)
+def send_dataframe(update,df):
+    # Create a sample DataFrame
+    # Convert the DataFrame to an image
+    fig, ax = plt.subplots()
+    ax.axis('off')
+    ax.table(cellText=df.values, colLabels=df.columns, cellLoc='center', loc='center')
+    plt.tight_layout()
+    buffer = BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+
+    # Send the DataFrame image as a photo message
+    bot.send_photo(chat_id=update, photo=buffer)
+
+def send_dataframe_as_table(chat_id, dataframe):
+                        # Convert DataFrame to string with tabulate
+                        table_string = tabulate(dataframe, headers='keys', tablefmt='fancy_grid')
+                        table_string =table_string.replace('+', '＋').replace('-', '－').replace('|', '｜')
+                        # Send the table string as a message
+                        bot.send_message(chat_id, f'```\n{table_string}\n```', parse_mode='Markdown')
+
+
+#logout method for logging out from API
+@bot.message_handler(commands=['logout'])
+def logout(message):
+    ret = api.logout()
+    if ret['stat']=='Ok':
+        bot.send_message(message.chat.id, 'API Logged Out')
+    else:
+        bot.send_message(message.chat.id, 'API is unable to Logged Out')
+                        
+#end method stopping the RMS System
+@bot.message_handler(commands=['end'])
+def end(message):
+    bot.send_message(message.chat.id, 'Enter 0 for exit:')
+    bot.register_next_step_handler(message, update_exit)
+def update_exit(message):
+    chat_id = message.chat.id
+    global exit
+    exit = message.text
+    if exit=='0':
+        bot.send_message(message.chat.id, 'Set Exit 0')
+    else :
+        bot.send_message(message.chat.id, 'Reset Exit, Started RMS')
+@bot.message_handler(commands=['start'])
+def start(message):
+    # Create the main menu with nested commands
+    markup = types.InlineKeyboardMarkup(row_width=3)
+    item1 = types.InlineKeyboardButton('Login Details', callback_data='Login Details')
+    item2 = types.InlineKeyboardButton('Holding', callback_data='Profit Booking')
+    item4 = types.InlineKeyboardButton('Position', callback_data='Position')
+    item3 = types.InlineKeyboardButton('RMS', callback_data='RMS')
+    markup.add(item1, item2, item3, item4)
+    try :
+          api is None  
+    except NameError:
+          bot.send_message(message.chat.id, 'API Not connected')
+    
+
+    bot.send_message(message.chat.id, 'Main Menu', reply_markup=markup)
+    
+    
+    
+@bot.message_handler(commands=['index'])
+def index_select(message):
+    # Create the main menu with nested commands
+    markup = types.InlineKeyboardMarkup(row_width=3)
+    item1 = types.InlineKeyboardButton('Portfolio', callback_data='Portfolio')
+    item2 = types.InlineKeyboardButton('BANK', callback_data='BANK')
+    item4 = types.InlineKeyboardButton('FIN', callback_data='FIN')
+    markup.add(item1, item2, item4)
+    
+    bot.send_message(message.chat.id, 'Index Selection', reply_markup=markup)
+       
+@bot.message_handler(commands=['index'])
+def sl_update(message):
+    bot.send_message(message.chat.id, 'Select the index: FIN BANK or ALL:')
+    bot.register_next_step_handler(message, update_sl)
+
+def update_sl(message): 
+    chat_id = message.chat.id
+    global sl
+    sl = float(message.text)
+    bot.send_message(message.chat.id, f'SL has been set to {sl}')
+
+@bot.message_handler(commands=['SL'])
+def sl_update(message):
+    bot.send_message(message.chat.id, 'Enter SL:')
+    bot.register_next_step_handler(message, update_sl)
+
+def update_sl(message): 
+    chat_id = message.chat.id
+    global sl
+    sl = float(message.text)
+    bot.send_message(message.chat.id, f'SL has been set to {sl}')
+
+@bot.message_handler(commands=['Login'])
+def login_shoonya(message):
+    bot.send_message(message.chat.id, 'Enter OTP:')
+    print(message)
+    bot.register_next_step_handler(message, perform_login)
+
+def perform_login(message):
+    chat_id = message.chat.id
+    otp = message.text
+    client=shoonya(twofa=otp,client_id='2')
+    api=client.login()
+    print(api)
+    if type(api)==str:
+           bot.send_message(message.chat.id, api)
+    else:
+           bot.send_message(message.chat.id, 'Logged In')
+    
+    
+    #bot.send_message(chat_id, 'Logged In')
+
+@bot.callback_query_handler(func=lambda call: True )
+def callback_handler(call):
+    global scrip
+    if call.data == 'Login Details':
+    # Echo the user's message
+                margin=api.get_limits()
+                status=margin['stat']
+                if status=='Ok':
+                    cash_margin=margin['cash']
+                    send_text=f'API Status : {status} & Cash Margin : {cash_margin}'
+                    bot.send_message(call.message.chat.id, send_text)
                 
+                else:
+                    bot.send_message(call.message.chat.id, f'API :  {status}')
+                
+            #else:
+           #    bot.send_message(call.message.chat.id, 'Enter SL : ')
+           #    chat_id = message.chat.id
+          #    text = message.text
+          #    global sl
+          #    sl=float(text)
+          #    print(sl)
+          #    bot.send_message(call.message.chat.id, 'Set SL')
+   #
+    elif call.data=='Portfolio':
+        #global scrip
+        scrip='ALL'
+        bot.send_message(call.message.chat.id, 'Index: Portfolio')
+        
+    elif call.data=='FIN':
+        #global scrip
+        scrip='FIN'
+        bot.send_message(call.message.chat.id, 'Index: FIN')
+    elif call.data=='BANK':
+        #global scrip
+        scrip='BANK'
+        bot.send_message(call.message.chat.id, 'Index: BANK NIFTY')
+        
+    elif call.data=='Profit Booking':
+        df1=pd.DataFrame(api.get_positions())
+        if df1.shape[0]==0:
+            bot.send_message(call.message.chat.id, 'There is No Position ')
+        else:
+            df1=df1[df1['exch']=='NFO'][df1['netqty']!='0']
+          
+        df1['CALL_PUT'] = np.where(df1['tsym'].str.contains('C', 'CE',
+                                    'PE'))
+        df1[['tsym','lp','CALL_PUT']]
+        ce=df1[df1['CALL_PUT']=='CE'][df1['lp']==df['lp'].max()].iloc[0][0]
+        pe=df1[df1['CALL_PUT']=='PE'][df1['lp']==df['lp'].max()].iloc[0][0]
+        if scrip=='BANK':
+            quantity=25
+        else:
+            quantity=40
+        api.place_order(buy_or_sell='B'
+                                , product_type='M',
+                                    exchange='NFO', tradingsymbol=ce, 
+                                    quantity=quantity, discloseqty=0,price_type='MKT', #price=0.1,# trigger_price=199.50,
+                                    retention='DAY', remarks='my_algo_order')
+        api.place_order(buy_or_sell='B'
+                                , product_type='M',
+                                    exchange='NFO', tradingsymbol=pe, 
+                                    quantity=quantity, discloseqty=0,price_type='MKT', #price=0.1,# trigger_price=199.50,
+                                    retention='DAY', remarks='my_algo_order')
+            
+        bot.send_message(call.message.chat.id, 'Reduced Risk / Profit Booked by 1 lot')
+    elif call.data=='Position':
+        
+            df1=pd.DataFrame(api.get_positions())
+            if df1.shape[0]==0:
+                bot.send_message(call.message.chat.id, 'There is No Position ')
+                if exit==0:
+                     bot.send_message(call.message.chat.id, f'RMS - Not on run, fixed SL  :{sl} and Index : {scrip}')
+                else: 
+                     bot.send_message(call.message.chat.id, f'RMS - Running with fixed SL as : {sl} and Index : {scrip}')
+                
+            else:
+                
+                df1=df1[df1['exch']=='NFO'][df1['netqty']!='0']
+                df1[['urmtom', 'rpnl','netqty','netavgprc']] = df1[['urmtom', 'rpnl','netqty','netavgprc']].apply(pd.to_numeric)
+                df1['Buy_Sell'] = np.where(df1['netqty']>0, 'BUY',
+                                   np.where(df1['netqty']<0, 'SELL','Other'
+                                    ))
+                df1['Instrument'] = np.where(df1['tsym'].str.contains('BANK'), 'BANKNIFTY',
+                                   np.where(df1['tsym'].str.contains('FIN'), 'FINNIFTY',
+                                    'Stock'))
+                df1=df1[['Instrument','Buy_Sell','netqty','urmtom','rpnl']]
+                
+                grouped = df1.groupby(['Instrument','Buy_Sell']).sum()
+                grouped[['Instrument', 'Buy_Sell']]=grouped.index
+                #send_dataframe_as_table(call.message.chat.id, grouped)
+                send_dataframe(call.message.chat.id,grouped)
+                
+                if exit==0:
+                     bot.send_message(call.message.chat.id, f'RMS - Not on run, fixed SL  :{sl} and Index : {scrip}')
+                else: 
+                     bot.send_message(call.message.chat.id, f'RMS - Running with fixed SL as : {sl} and Index : {scrip}')
+                
+            
+    #elif call.data == 'API Health':
+    #   margin=api.get_limits()
+    #   
+    #   send='API Response: Status' + margin['stat']+'& the'+'cash used '+ margin['cash']
+    #   bot.send_message(call.message.chat.id, send)
+    #   return
+    elif call.data == 'RMS':
+        # Create the submenu with nested commands
+        submenu_markup = types.InlineKeyboardMarkup(row_width=2)
+        submenu_item1 = types.InlineKeyboardButton('SHOW_KPI', callback_data='SHOW_KPI')
+        submenu_item2 = types.InlineKeyboardButton('Position', callback_data='Position')
+        submenu_item3 = types.InlineKeyboardButton('Update_SL', callback_data='Update_SL')
+      #  submenu_item4 = types.InlineKeyboardButton('index', callback_data='RMS')
+        submenu_markup.add( submenu_item1,  submenu_item2,  submenu_item3)
+    
+        bot.send_message(call.message.chat.id, 'Main Menu', reply_markup=submenu_markup)
+       
+    ## make changesin th shpw kpi for taking scrip along with operation       
+    elif call.data=='SHOW_KPI':
+ #        bot.send_message(call.message.chat.id, 'Show KPI')
+# Script setting up for the code
+        
+        while True:
+            #except requests.exceptions.ConnectionError as e:
+            show=update_strategy_performance(scrip, sl)
+            if show[0]=='No':
+                
+                bot.send_message(call.message.chat.id, 'Position Started Exiting')
+                
+                ret = api.get_order_book()
+
+                for i in ret['norenordno'][ret['status']=='OPEN']:
+                    x=api.cancel_order(orderno=i)
+                    
+                exit='0'
+                break
+            else:
+                Net_credit = show[4]
+                Net_PL = show[0]
+                CURRENT = show[1]
+                stop_loss = show[2]
+                margin = show[3]
+                sl_in_cash = round(stop_loss * (margin / 100),2)
+      
+                
+                            
+                data = {
+                    'RMS Vaues': ['Fixed SL', '% P/L', 'Net Profit','Net Credit','SL in Cash','Booked PL','Un realised PL'],
+                    'Values': [stop_loss,Net_PL,CURRENT,Net_credit,sl_in_cash,show[5],show[6]],
+                     
+                }
+                
+                # Create a DataFrame from the dictionary
+                df = pd.DataFrame(data)
+                send_dataframe(call.message.chat.id,df)
+               # send_dataframe_as_table(call.message.chat.id, df)
+                #bot.send_message(call.message.chat.id, 'Set Stop Loss :' +str(stop_loss)+'& Net P/L : '+str(Net_PL)+'  & Net Credit Amount = '+str(Net_credit))
+                time.sleep(15)  # Delay for 15 seconds
+                print('checker1')
+           # if exit=='0':
+            #    break
+        if exit=='0':
+                bot.send_message(call.message.chat.id, 'RMS Stopped with Exiting the Position')
+        
+                              
+    bot.send_message(message.chat.id, 'Main Menu', reply_markup=markup)
+bot.infinity_polling(timeout=10, long_polling_timeout = 10)
                 
 
 
